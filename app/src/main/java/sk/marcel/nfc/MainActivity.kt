39c9558fa869bf493b448ac78f55e241bac8fa8c
Marcel Palaj <palaj.marcel@gmail.com>
2022-03-22 22:05:12 +0100

    create reading pictures from json, start working on progress, display needed colors

--- app/src/main/java/sk/marcel/nfc/MainActivity.kt
+++ app/src/main/java/sk/marcel/nfc/MainActivity.kt
@@ -2,50 +2,111 @@ package sk.marcel.nfc
 
 import android.content.Intent
 import android.content.res.Resources
+import android.graphics.Color
 import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.switchmaterial.SwitchMaterial
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
     private var mNfcAdapter: NfcAdapter? = null
+    private var needColors = ArrayList<String>()
+    private val neededColorsToColor = mapOf(
+        Pair("red", "#ff0000"),
+        Pair("green", "#00ff00"),
+        Pair("blue", "#0000ff"),
+        Pair("yellow", "#f6ff00"),
+        Pair("cyan", "#00ddff"),
+        Pair("magenta", "#ff00a2"),
+        Pair("orange", "#ff7b00"),
+        Pair("white", "#ffffff"),
+        Pair("brown", "#853100"),
+        Pair("light green", "#a9ff91"),
+        Pair("dark green", "#0e4200"),
+        Pair("azure", "#628bc4"),
+        Pair("purple", "#8d0aff")
+    )
+
+    private fun displayNeededColors(){
+        val grid = GridLayout(this)
+        grid.columnCount = 4
+        grid.layoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        for(color in needColors){
+            val colorView = this.layoutInflater.inflate(R.layout.needed_color, findViewById(R.id.need_colors), false)
+            colorView.findViewById<ImageView>(R.id.color_image_view).setBackgroundColor(Color.parseColor(neededColorsToColor[color]))
+            colorView.findViewById<TextView>(R.id.color_text_view).text = color
+            grid.addView(colorView)
+        }
+        findViewById<FrameLayout>(R.id.need_colors).addView(grid)
+    }
+
+    private fun getPicturesJson(i:Int): JSONArray? {
+        BufferedReader(assets.open("pictures.json").reader()).use { reader ->
+            return JSONArray(reader.readText()).getJSONArray(i)
+        }
+    }
+
+    private fun getProgressJson(): JSONArray {
+        BufferedReader(File(this.filesDir.absolutePath, "progress.json").reader()).use { reader ->
+            return JSONArray(reader.readText())
+        }
+    }
+
+    private fun makePixelView(dim: Int, pixel: JSONObject): ImageView {
+        val pixelView = ImageView(this)
+        pixelView.layoutParams = ViewGroup.LayoutParams(dim, dim)
+        if(pixel.getBoolean("precolored"))
+            pixelView.setBackgroundColor(Color.parseColor(pixel.getString("display_color")))
+        else{
+            needColors.add(pixel.getString("need_color"))
+            pixelView.setBackgroundColor(Color.parseColor("#22"+(pixel.getString("display_color")).substring(1)))
+        }
+        return pixelView
+    }
+
+    private fun createPixels(){
+        needColors = ArrayList()
+        val json = getPicturesJson(1) ?: return
+        val frameLayout = findViewById<FrameLayout>(R.id.frame)
+        frameLayout.removeAllViews()
+
+        val grid = GridLayout(this)
+        grid.rowCount = json.length()
+        grid.columnCount = json.getJSONArray(0).length()
+        grid.layoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+
+        val dim = minOf(
+            (Resources.getSystem().displayMetrics.widthPixels)/grid.columnCount,
+            Resources.getSystem().displayMetrics.widthPixels/ grid.rowCount
+        )
+
+        for(i in 0 until grid.rowCount)
+            for(j in 0 until grid.columnCount)
+                grid.addView(makePixelView(dim, json.getJSONArray(i).getJSONObject(j)))
+
+        frameLayout.addView(grid)
+    }
+
+    private fun initProgress(){
+        File(this.filesDir.absolutePath, "progress.json").createNewFile()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        initProgress()
         createPixels()
-    }
-
-    private fun createPixels(){
-        val frameLayout = findViewById<FrameLayout>(R.id.frame)
-        frameLayout.removeAllViews()
-        val rowCount = 5
-        val colCount = 4
-        val grid = GridLayout(this)
-        grid.columnCount = colCount
-        grid.rowCount = rowCount
-        val dim = minOf(Resources.getSystem().displayMetrics.widthPixels/colCount, Resources.getSystem().displayMetrics.widthPixels/ colCount)
-        grid.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        for(i in 0 until rowCount){
-            for(j in 0 until colCount){
-                val pixel = ImageView(this)
-                pixel.layoutParams = ViewGroup.LayoutParams(dim, dim)
-                if((i+j)%2==0)
-                    pixel.setBackgroundColor(getColor(R.color.purple_200))
-                else
-                    pixel.setBackgroundColor(getColor(R.color.teal_700))
-                grid.addView(pixel)
-            }
-        }
-        frameLayout.addView(grid)
+        displayNeededColors()
     }
 
     override fun onResume() {
