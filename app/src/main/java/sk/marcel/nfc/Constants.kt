c886fd912f2384d60f77103123c45dcd141354f6
Marcel Palaj <palaj.marcel@gmail.com>
2022-03-23 09:50:16 +0100

    add progress json, add dummy color add

---
+++ app/src/main/java/sk/marcel/nfc/Constants.kt
@@ -0,0 +1,23 @@
+package sk.marcel.nfc
+
+object Constants {
+        const val precolored = "precolored"
+        const val displayColor = "display_color"
+        const val needColor = "need_color"
+        const val alpha = "#22"
+        val colorsMap = mapOf(
+            Pair("red", "#ff0000"),
+            Pair("green", "#00ff00"),
+            Pair("blue", "#0000ff"),
+            Pair("yellow", "#f6ff00"),
+            Pair("cyan", "#00ddff"),
+            Pair("magenta", "#ff00a2"),
+            Pair("orange", "#ff7b00"),
+            Pair("white", "#ffffff"),
+            Pair("brown", "#853100"),
+            Pair("light green", "#a9ff91"),
+            Pair("dark green", "#0e4200"),
+            Pair("azure", "#628bc4"),
+            Pair("purple", "#8d0aff")
+        )
+}
--- app/src/main/java/sk/marcel/nfc/MainActivity.kt
+++ app/src/main/java/sk/marcel/nfc/MainActivity.kt
@@ -1,81 +1,58 @@
 package sk.marcel.nfc
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.res.Resources
 import android.graphics.Color
 import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.view.View
 import android.view.ViewGroup
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.switchmaterial.SwitchMaterial
-import org.json.JSONArray
 import org.json.JSONObject
-import java.io.BufferedReader
-import java.io.File
 
 
 class MainActivity : AppCompatActivity() {
 
     private var mNfcAdapter: NfcAdapter? = null
-    private var needColors = ArrayList<String>()
-    private val neededColorsToColor = mapOf(
-        Pair("red", "#ff0000"),
-        Pair("green", "#00ff00"),
-        Pair("blue", "#0000ff"),
-        Pair("yellow", "#f6ff00"),
-        Pair("cyan", "#00ddff"),
-        Pair("magenta", "#ff00a2"),
-        Pair("orange", "#ff7b00"),
-        Pair("white", "#ffffff"),
-        Pair("brown", "#853100"),
-        Pair("light green", "#a9ff91"),
-        Pair("dark green", "#0e4200"),
-        Pair("azure", "#628bc4"),
-        Pair("purple", "#8d0aff")
-    )
+    private var needColors = HashMap<Pair<Int, Int>, String>()
+    private lateinit var jsonsHelpers: JsonsHelpers
 
     private fun displayNeededColors(){
+        findViewById<FrameLayout>(R.id.need_colors).removeAllViews()
         val grid = GridLayout(this)
-        grid.columnCount = 4
+        grid.columnCount = 3
         grid.layoutParams = ViewGroup.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
-        for(color in needColors){
+        for(color in needColors.values){
             val colorView = this.layoutInflater.inflate(R.layout.needed_color, findViewById(R.id.need_colors), false)
-            colorView.findViewById<ImageView>(R.id.color_image_view).setBackgroundColor(Color.parseColor(neededColorsToColor[color]))
+            colorView.findViewById<ImageView>(R.id.color_image_view).setBackgroundColor(Color.parseColor(Constants.colorsMap[color]))
             colorView.findViewById<TextView>(R.id.color_text_view).text = color
             grid.addView(colorView)
         }
         findViewById<FrameLayout>(R.id.need_colors).addView(grid)
     }
 
-    private fun getPicturesJson(i:Int): JSONArray? {
-        BufferedReader(assets.open("pictures.json").reader()).use { reader ->
-            return JSONArray(reader.readText()).getJSONArray(i)
-        }
-    }
-
-    private fun getProgressJson(): JSONArray {
-        BufferedReader(File(this.filesDir.absolutePath, "progress.json").reader()).use { reader ->
-            return JSONArray(reader.readText())
-        }
-    }
-
-    private fun makePixelView(dim: Int, pixel: JSONObject): ImageView {
+    private fun makePixelView(dim: Int, pixel: JSONObject, i: Int, j: Int): ImageView {
         val pixelView = ImageView(this)
+        val coloredPixels = jsonsHelpers.getColoredPixels()
         pixelView.layoutParams = ViewGroup.LayoutParams(dim, dim)
-        if(pixel.getBoolean("precolored"))
-            pixelView.setBackgroundColor(Color.parseColor(pixel.getString("display_color")))
+        if(pixel.getBoolean(Constants.precolored) || coloredPixels.contains(Pair(i,j)))
+            pixelView.setBackgroundColor(Color.parseColor(pixel.getString(Constants.displayColor)))
         else{
-            needColors.add(pixel.getString("need_color"))
-            pixelView.setBackgroundColor(Color.parseColor("#22"+(pixel.getString("display_color")).substring(1)))
+            needColors[Pair(i,j)] = pixel.getString(Constants.needColor)
+            pixelView.setBackgroundColor(Color.parseColor(Constants.alpha+(pixel.getString(Constants.displayColor)).substring(1)))
         }
         return pixelView
     }
 
     private fun createPixels(){
-        needColors = ArrayList()
-        val json = getPicturesJson(1) ?: return
+        needColors = HashMap()
+        val json = jsonsHelpers.getPicturesJson(jsonsHelpers.getProgressNum()) ?: return
         val frameLayout = findViewById<FrameLayout>(R.id.frame)
         frameLayout.removeAllViews()
 
@@ -91,20 +68,56 @@ class MainActivity : AppCompatActivity() {
 
         for(i in 0 until grid.rowCount)
             for(j in 0 until grid.columnCount)
-                grid.addView(makePixelView(dim, json.getJSONArray(i).getJSONObject(j)))
+                grid.addView(makePixelView(dim, json.getJSONArray(i).getJSONObject(j), i, j))
 
         frameLayout.addView(grid)
     }
 
-    private fun initProgress(){
-        File(this.filesDir.absolutePath, "progress.json").createNewFile()
+    fun addColorOnClick(v: View){
+        val colorTextView = findViewById<EditText>(R.id.color)
+        val addedColor = colorTextView.text.toString()
+        colorTextView.text = SpannableStringBuilder("")
+
+        addColor(addedColor)
+    }
+
+    fun addColor(color: String){
+        for(key in needColors.keys){
+            if(needColors[key] == color) {
+                needColors.remove(key)
+                jsonsHelpers.addSolvedColor(key.first, key.second)
+                break
+            }
+        }
+        if(needColors.isEmpty()){
+            newPicture()
+        }
+        createPixels()
+        displayNeededColors()
+    }
+
+    fun newPicture(){
+        jsonsHelpers.startNewPicture()
+    }
+
+    fun resetProgressFile(v: View){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Reset?")
+            .setPositiveButton("Yes") { _: DialogInterface?, _: Int ->
+                jsonsHelpers.resetProgressFile()
+                createPixels()
+                displayNeededColors()
+            }
+            .setNegativeButton("No") { dialog: DialogInterface, _: Int -> dialog.cancel() }
+        val alert = builder.create()
+        alert.show()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        initProgress()
+        jsonsHelpers = JsonsHelpers(this)
         createPixels()
         displayNeededColors()
     }
